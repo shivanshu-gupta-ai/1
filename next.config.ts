997a09055b931fb0eaadcadc114c4edf48d9d666
shivanshu-gupta-ai <shivanshu.ai.biz@gmail.com>
2025-05-19 09:33:02 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = "Shivanshu-portfolio"; 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
+  output: "export", 
 };
 
 export default nextConfig;
